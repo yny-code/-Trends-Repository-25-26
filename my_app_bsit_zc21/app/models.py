5113d963854b0b05be359a6aa230e0d3d68b354a
Eda Rose M. Ignacio <erignacio@gbox.adnu.edu.ph>
2026-05-12 09:45:32 +0800

    Add files via upload

---
+++ my_app_bsit_zc21/app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
